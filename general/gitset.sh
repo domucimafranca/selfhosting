104409717749503a1bf843285d59b468e8bbce61
Domu <147288569+domucimafranca@users.noreply.github.com>
2024-09-28 22:56:18 +0800

    Update gitset.sh

--- general/gitset.sh
+++ general/gitset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git config --global user.email "domu@cimafranca.com"
+git config --global user.email "someone@somewhere.com"
 git config --global user.name "Domu Cimafranca"
 git config --global core.editor "vim"
